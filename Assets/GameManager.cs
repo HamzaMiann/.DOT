4c03ad167090e39a221060d800d61a13527dc315
Hamza Mian <hamza.mian@hotmail.com>
2018-07-09 22:00:30 -0400

    Initial Commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour{
+
+    public GameObject obj;
+    public static int SpawnQueue = 0;
+    private static GameObject objectToSpawn;
+
+    private void Start()
+    {
+        objectToSpawn = obj;
+        SpawnQueue = 50;
+        createNewObjects();
+    }
+
+    private static void createNewObjects()
+    {
+        for (; SpawnQueue > 0; SpawnQueue--)
+        {
+            Instantiate(objectToSpawn);
+        }
+    }
+
+    public void Update()
+    {
+        createNewObjects();
+    }
+
+}
---
+++ Assets/MenuScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour
+{
+
+    public GameObject menu; // Assign in inspector
+    private bool isShowing;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            isShowing = !isShowing;
+            menu.SetActive(isShowing);
+        }
+    }
+
+    public void hide()
+    {
+        isShowing = false;
+        menu.SetActive(false);
+        GetComponent<AudioSource>().Play();
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
